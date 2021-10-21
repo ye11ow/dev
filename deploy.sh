14dc98b4bcd2c3849830962094a095048fe06c6e
ye11ow <ye111111ow@gmail.com>
2021-10-20 18:36:02 -0700

    improved deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ check_command() {
 		return 0
 	else
 		echo "command $1 does not exist"
-		return 1
+        exit 1
 	fi
 }
 
@@ -19,11 +19,18 @@ fi
 
 if check_command zsh; then
     echo "installing ohmyzsh and powerlevel10k theme..."
+
+    # install ohmyzsh
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+    # install powerlevel10k
     git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
 	echo "configurating zsh..."
 	if ! grep -q "source\ ~/profiles/.bashrc" ~/.zshrc; then
 	    echo "source ~/profiles/.bashrc" >> ~/.zshrc
 	fi
-	echo "zsh configured"
+
+    # https://github.com/romkatv/powerlevel10k#oh-my-zsh
+	echo "zsh configured, please manually set ZSH_THEME='powerlevel10k/powerlevel10k' in ~/.zshrc and then restart zsh"
 fi
